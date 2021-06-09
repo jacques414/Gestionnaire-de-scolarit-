9952a68ab9fc2804febdfe8a05b76b8485e43224
jacques414 <59058145+jacques414@users.noreply.github.com>
2021-06-09 23:21:58 +0200

    Add files via upload

---
+++ gestion de scolarite/index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+mysql_connect("localhost", "root", "");
+mysql_select_db("gestion");
+?>
+<html>
+<body>
+<?php
+include('cadre.php');
+?>
+<div class="corp">
+</div>
+</body>
+</html>
